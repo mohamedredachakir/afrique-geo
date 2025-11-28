2dce612dc740b2a4d3e2624ac5ba79395869d5b0
mohamedredachakir <chakirm082@gmail.com>
2025-11-28 16:24:12 +0100

    add some function in file country.php for take and update and delete country

---
+++ src/Country.php
@@ -0,0 +1,39 @@
+
+
+<?php
+
+require_once __DIR__ . '/db.php';
+
+
+
+
+function getAllCountries() {
+   $stmt = $pdo->query("SELECT * FROM countries ORDER BY name ");
+    return $stmt->fetchAll();
+}
+
+
+
+function getCountryById($pdo,$id) {
+    $stmt= $pdo->prepare("SELECT * FROM countries WHERE id = :id");
+    $stmt->execute(['id' => $id]);
+    return $stmt->fetch();
+}
+
+
+function addCountry($pdo, $name, $population, $languages) {
+    $stmt = $pdo->prepare("INSERT INTO countries (name, population, languages) VALUES (?, ?, ?)");
+    return $stmt->execute([$name, $population, $languages]);
+}
+
+
+function updateCountry($pdo, $id, $name, $population, $languages) {
+    $stmt = $pdo->prepare("UPDATE countries SET name = ?, population = ?, languages = ? WHERE id = ?");
+    return $stmt->execute([$name, $population, $languages, $id]);
+}
+
+
+function deleteCountry($pdo, $id) {
+    $stmt = $pdo->prepare("DELETE FROM countries WHERE id = ?");
+    return $stmt->execute([$id]);
+}
